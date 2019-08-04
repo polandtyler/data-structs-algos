e488f9a913042c0c7a377aaf2ea5e8e2f6d1e54d
Tyler Poland <poland.tyler@gmail.com>
2019-08-04 11:09:34 -0500

    QuickSort in Java - Lomuto’s partition algorithm

--- java/src/QuickSort.java
+++ java/src/QuickSort.java
@@ -1,52 +1,14 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class QuickSort {
 
     public static void main(String[] args) {
-        Integer[] ints = { 9, 2, 5, 3, 1254, 603, 13, 15, 1, 3, 4, 6};
-        Integer[] dupes = { 4,3,1,2,3 };
+        Integer[] ints = { 10, 0, 3, 9, 2, 14, 8, 27, 1, 5, 8, 3, -1, 26 };
         String[] strings = {"Zebra", "Xylophone", "Enjoy", "Llama", "King Arthur", "Lannister", "America", "America"};
 
-        System.out.println(Arrays.toString(sort(ints, 0, ints.length - 1)));
-        System.out.println(Arrays.toString(sort(strings, 0, strings.length - 1)));
-        System.out.println(Arrays.toString(sort(dupes, 0, dupes.length - 1)));
-    }
-
-
-    private static <T extends Comparable<T>> T[] sort(T[] arr, int start, int end) {
-        int partition = partition(arr, start, end);
-
-        if (partition-1 > start) {
-            sort(arr, start, partition - 1);
-        }
-
-        if (partition + 1 < end) {
-            sort(arr, partition + 1, end);
-        }
-
-        return arr;
-    }
-
-    private static <T extends Comparable<T>> int partition(T[] arr, int start, int end) {
-        T pivot = arr[start + (end-start) / 2];
-
-        for (int i=start;i<end;i++) {
-            if (arr[i].compareTo(pivot) <= 0) {
-                T temp = arr[start];
-                arr[start] = arr[i];
-                arr[i] = temp;
-                start++;
-            }
-        }
-
-        T temp = arr[start];
-        arr[start] = pivot;
-        arr[end] = temp;
-
-        return start;
+        System.out.println(Arrays.toString(quickSortHoare(ints, 0, ints.length - 1)));
+        System.out.println(Arrays.toString(quickSortHoare(strings, 0, strings.length - 1)));
     }
 
     /*
@@ -87,12 +49,28 @@ public class QuickSort {
     Partitioning using Lomuto's algorithm to find the pivot
      */
     public static <T extends Comparable<T>> int partitionLomuto(T[] array, int start, int end) {
-        // TODO: implementation
+        T pivot = array[end];
+
+        for (int i = start; i <= end; i++) {
+            if (array[i].compareTo(pivot) < 0) {
+                array[start] = array[i];
+                array[i] = array[start];
+                start += 1;
+            }
+        }
+
+        array[start] = array[end];
+        array[end] = array[start];
+
+        return start;
     }
 
     public static <T extends Comparable<T>> T[] quickSortLomuto(T[] array, int start, int end) {
-
-        // TODO: implementation
+        if (start < end) {
+            int pivot = partitionLomuto(array, start, end);
+            quickSortLomuto(array, start, pivot - 1);
+            quickSortLomuto(array, pivot + 1, end);
+        }
 
         return array;
     }
